85d0c57dbb9b1b60948fd98d222ef185a05dde59
Mecha Ophidian <152424633+mogitdb@users.noreply.github.com>
2024-07-17 14:29:14 -0400

    Front_sprite
    +Tween to manage shrink & grow animation
    +Tween looped to repeat animation

---
+++ front_sprite.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+
+const SCALE_SMALL: Vector2 = Vector2(0.1, 0.1)
+const SCALE_NORMAL: Vector2 = Vector2(1.0, 1.0)
+const SCALE_TIME: float = 0.5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	run_me()
+
+
+func run_me() -> void:
+	var tween = get_tree().create_tween()
+	tween.set_loops()
+	tween.tween_property(self, "scale", SCALE_NORMAL, SCALE_TIME)
+	tween.tween_property(self, "scale", SCALE_SMALL, SCALE_TIME)
+
