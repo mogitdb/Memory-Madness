9d4e1b03db04dfa7b6b19e6e69af988ff08b9ad7
Mecha Ophidian <152424633+mogitdb@users.noreply.github.com>
2024-07-19 20:13:29 -0400

    Tile Selection
    +Tiles start out hidden
    +Tiles reveal when clicked

--- scenes/memory_tile/memory_tile.gd
+++ scenes/memory_tile/memory_tile.gd
@@ -5,17 +5,38 @@ extends TextureButton
 
 
 var _item_name: String
+var _can_select_me: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalManager.on_selection_disabled.connect(on_selection_disabled)
+	SignalManager.on_selection_enabled.connect(on_selection_enabled)
 
 
 func get_item_name() -> String:
 	return _item_name
-	
+
+
+func reveal(r: bool) -> void:
+	frame_image.visible = r
+	item_image.visible = r
+
 
 func setup(ii_dict: Dictionary, fi: CompressedTexture2D) -> void:
 	frame_image.texture = fi
 	item_image.texture = ii_dict.item_texture
 	_item_name = ii_dict.item_name
+	reveal(false)
+
+
+func on_selection_disabled() -> void:
+	_can_select_me = false
+	
+
+func on_selection_enabled() -> void:
+	_can_select_me = true
+
+
+func _on_pressed():
+	if _can_select_me == true:
+		reveal(true)
--- singletons/signal_manager.gd
+++ singletons/signal_manager.gd
@@ -2,4 +2,6 @@ extends Node
 
 signal on_level_selected(level_num: int)
 signal on_game_exit_pressed
+signal on_selection_enabled
+signal on_selection_disabled
 
